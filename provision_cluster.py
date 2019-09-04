7d5cda21f84cfdd75c02f75a0f2d299ee0d1f752
Iain Bromley <iaineubromley@gmail.com>
2019-09-03 23:34:48 -0700

    Add infrastructure provisioning code
    Separated AWS credentials to separate unchecked file and added
    attributes to dwh.cfg to support IaC provisioning. Unsure if the logic
    will need to be merged into create_tables.py.

---
+++ provision_cluster.py
@@ -0,0 +1,27 @@
+import configparser
+import boto3
+
+config = configparser.ConfigParser()
+config.read(['aws.cfg', 'dwh.cfg'])
+
+KEY = config.get('AWS','KEY')
+SECRET = config.get('AWS','SECRET')
+ 
+redshift = boto3.client('redshift',
+                       region_name="us-west-2",
+                       aws_access_key_id=KEY,
+                       aws_secret_access_key=SECRET
+                       )
+
+try:
+    response = redshift.create_cluster(        
+        ClusterType=config.get("INFRA","DWH_CLUSTER_TYPE"),
+        NodeType=config.get("INFRA","DWH_NODE_TYPE"),
+        NumberOfNodes=int(config.get("INFRA","DWH_NUM_NODES")),
+        ClusterIdentifier=config.get("INFRA","DWH_CLUSTER_IDENTIFIER"),
+        DBName=config.get("CLUSTER","DB_NAME"),
+        MasterUsername=config.get("CLUSTER","DB_USER"),
+        MasterUserPassword=config.get("CLUSTER","DB_PASSWORD")
+    )
+except Exception as e:
+    print(e)
